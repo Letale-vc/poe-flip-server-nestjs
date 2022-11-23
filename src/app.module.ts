f99b0ac63da0b82b2f3f57776df4e0e10f131ddb
Letale-vc <letalerv@gmail.com>
2022-11-23 12:31:16 +0200

    just rename folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { CardPoeDataModule } from './card-poe-data/card-poe-data.module';
+import { FlipQueriesModule } from './flip-queries/flip-queries-module';
 import { PoeDataModule } from './poe-data/poe-data.module';
 import { PoeFetchModule } from './poe-fetch/poe-fetch.module';
-import { FlipQueriesModule } from './poe-queries/flip-queries-module';
 
 @Module({
   imports: [
